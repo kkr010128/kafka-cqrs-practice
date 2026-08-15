7a2a8d3b3cf8738aec6628ba2d9c408f35e8ca94
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2026-08-15 21:16:48 +0900

    read 프로젝트 생성

---
+++ kafka_reader/src/main/java/com/hyun/kafka_reader/BookController.java
@@ -0,0 +1,37 @@
+package com.hyun.kafka_reader;
+
+import com.mongodb.client.*;
+import lombok.RequiredArgsConstructor;
+import org.bson.Document;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BookController {
+    @GetMapping("/cqrs/book")
+    public ResponseEntity<List> getBooks() {
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase database = mongoClient.getDatabase("cqrs");
+        MongoCollection<Document> mongo_books = database.getCollection("books");
+        List<Document> list = new ArrayList<Document>();
+
+        try {
+            try (MongoCursor<Document> cur = mongo_books.find().iterator()) {
+                while (cur.hasNext()) {
+                    Document doc = cur.next();
+                    list.add(doc);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            mongoClient.close();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+}
---
+++ kafka_reader/src/test/java/com/hyun/kafka_reader/KafkaReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyun.kafka_reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
